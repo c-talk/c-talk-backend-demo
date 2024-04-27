e38361c337aed440640b69ddaad4f55201a41ffe
haoduor <954040956@qq.com>
2024-04-27 16:10:51 +0800

    feat: 修改密码接口

--- src/main/java/me/a632079/ctalk/controller/UserController.java
+++ src/main/java/me/a632079/ctalk/controller/UserController.java
@@ -1,16 +1,18 @@
 package me.a632079.ctalk.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
+import ma.glasnost.orika.MapperFacade;
 import me.a632079.ctalk.po.User;
 import me.a632079.ctalk.repository.UserRepository;
 import me.a632079.ctalk.service.UserService;
-import me.a632079.ctalk.vo.PageVo;
-import me.a632079.ctalk.vo.UserPageForm;
-import me.a632079.ctalk.vo.UserVo;
+import me.a632079.ctalk.util.Argon2Util;
+import me.a632079.ctalk.vo.*;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @className: UserController
@@ -29,6 +31,12 @@ public class UserController {
     @Resource
     private UserRepository userRepository;
 
+    @Resource
+    private MapperFacade mapperFacade;
+
+    @Resource
+    private Argon2Util argon2Util;
+
     @GetMapping("/list")
     public List<User> list() {
         return userRepository.findAll();
@@ -39,8 +47,37 @@ public class UserController {
         return userService.pageUser(form);
     }
 
-    @PostMapping("/set/{id}")
-    public void set() {
+    @PostMapping("/set")
+    public void set(UserSetForm form) {
+        User user = mapperFacade.map(form, User.class);
+        user.setId(StpUtil.getLoginIdAsLong());
+
+        userRepository.save(user);
+    }
+
+    @PostMapping("/changePassword")
+    public void changePassword(ChangePasswordForm form) {
+        Optional<User> user = userRepository.findById(StpUtil.getLoginIdAsLong());
+        if (user.isEmpty()) {
+            return;
+        }
+
+        User u = user.get();
+
+        if (!argon2Util.verify(u, form.getOriPassword())) {
+            //TODO 异常
+            return;
+        }
+
+        if (!form.getPassword()
+                 .equals(form.getRePassword())) {
+            return;
+        }
+
+        u.setPassword(argon2Util.hash(form.getPassword()));
+        userRepository.save(u);
+
+        StpUtil.logout(u.getId());
     }
 
     @GetMapping("/get/{id}")
